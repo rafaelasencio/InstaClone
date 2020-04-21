fad388f764fb2285f4729a47de899b886c058bac
RafaelAsencio <rafaelasenciodeveloper@gmail.com>
2020-04-21 10:19:31 +0200

    Fetch notifications types for posts, comments and follows. Configure NotificationCell with appropriate notification type.

--- Spotsound/Controller/Notifications/NotificationsVC.swift
+++ Spotsound/Controller/Notifications/NotificationsVC.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import Firebase
+
 
 private let reuseIdentifier = "NotificationCell"
 
 class NotificationsVC: UITableViewController {
 
+    //MARK: - Properties
+    var notifications = [Notification]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,6 +25,9 @@ class NotificationsVC: UITableViewController {
         
         // register cell
         self.tableView.register(NotificationCell.self, forCellReuseIdentifier: reuseIdentifier)
+        
+        // fetch notifications
+        fetchNotification()
     }
     
     //MARK: TableView data source
@@ -29,12 +37,46 @@ class NotificationsVC: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return notifications.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! NotificationCell
+        cell.notification = notifications[indexPath.row]
         return cell
     }
+    
+    func fetchNotification(){
+        
+        guard let currentUserUid = Auth.auth().currentUser?.uid else {return}
+        
+        // .childAdded go through each notification
+        NOTIFICATIONS_REF.child(currentUserUid).observe(.childAdded) { (snapshot) in
+            
+            guard let dictionary = snapshot.value as? Dictionary <String, AnyObject> else {return}
+            guard let uid = dictionary["uid"] as? String else {return}
+            
+            Database.fetchUser(with: uid) { (user) in
+                
+                // if notification is for post
+                if let postId = dictionary["postId"] as? String {
+                    
+                    Database.fetchPost(with: postId) { (post) in
+                        let notification = Notification(user: user, post: post, dictionary: dictionary)
+                        self.notifications.append(notification)
+                        self.tableView.reloadData()
+                    }
+                } else {
+                    
+                    let notification = Notification(user: user, dictionary: dictionary)
+                    self.notifications.append(notification)
+                    self.tableView.reloadData()
+                }
+            }
+            
+        }
+    }
+    
+    
 }
--- Spotsound/View/NotificationCell.swift
+++ Spotsound/View/NotificationCell.swift
@@ -13,6 +13,26 @@ class NotificationCell: UITableViewCell {
     
     //MARK: - Properties
     
+    var notification: Notification? {
+        
+        didSet {
+            guard let user = notification?.user else { return }
+            guard let profileImageUrl = user.profileImageUrl else { return }
+            
+            // configure notification label
+            configureNotificationLabel()
+            
+            // configure notification type
+            configureNotificationType()
+            
+            profileImageView.loadImage(with: profileImageUrl)
+            
+            if let post = notification?.post {
+                postImageView.loadImage(with: post.imageUrl)
+            }
+        }
+    }
+    
     let profileImageView: CustomImageView = {
        let iv = CustomImageView()
         iv.contentMode = .scaleAspectFill
@@ -23,11 +43,6 @@ class NotificationCell: UITableViewCell {
     
     let notificationLabel: UILabel = {
         let lbl = UILabel()
-        let attributedText = NSMutableAttributedString(string: "username", attributes: [NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 12)])
-        attributedText.append(NSAttributedString(string: " commented on your post", attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 12)]))
-        attributedText.append(NSAttributedString(string: " 2d", attributes: [
-                       NSAttributedString.Key.font : UIFont.systemFont(ofSize: 12), NSAttributedString.Key.foregroundColor : UIColor.lightGray]))
-        lbl.attributedText = attributedText
         lbl.numberOfLines = 2
         return lbl
     }()
@@ -51,33 +66,67 @@ class NotificationCell: UITableViewCell {
     }()
     
     
-    // MARK: - Handler
+    // MARK: - Handlers
     
     @objc func handleFollowTapped(){
         print("handleFollowTapped")
     }
     
+    func configureNotificationLabel(){
+        
+        guard let notification = self.notification else { return }
+        guard let user = notification.user else { return }
+        guard let username = user.username else { return }
+        let notificationMessage = notification.notificationType.description
+        
+        let attributedText = NSMutableAttributedString(string: username, attributes: [NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 12)])
+        attributedText.append(NSAttributedString(string: notificationMessage, attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 12)]))
+        attributedText.append(NSAttributedString(string: " 2d", attributes: [
+        NSAttributedString.Key.font : UIFont.systemFont(ofSize: 12), NSAttributedString.Key.foregroundColor : UIColor.lightGray]))
+        notificationLabel.attributedText = attributedText
+    }
+    
+    func configureNotificationType(){
+        
+        guard let notification = self.notification else { return }
+        guard let user = notification.user else { return }
+        
+        var anchor: NSLayoutXAxisAnchor!
+        
+        if notification.notificationType != .Follow {
+            
+            // notification for comment or like
+            self.addSubview(postImageView)
+            postImageView.anchor(top: nil, left: nil, bottom: nil, right: self.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 8, width: 40, height: 40)
+            postImageView .centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+            anchor = postImageView.leftAnchor
+            
+        } else {
+            // notification for follow
+
+            addSubview(followButton)
+            followButton.anchor(top: nil, left: nil, bottom: nil, right: self.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 12, width: 90, height: 30)
+            followButton.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+            followButton.layer.cornerRadius = 3
+            anchor = followButton.leftAnchor
+        }
+        
+        self.addSubview(notificationLabel)
+        notificationLabel.anchor(top: nil, left: profileImageView.rightAnchor, bottom: nil, right: anchor, paddingTop: 0, paddingLeft: 8, paddingBottom: 0, paddingRight: 8, width: 0, height: 0)
+        notificationLabel .centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        
+    }
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        selectionStyle = .none
         
         self.addSubview(profileImageView)
         profileImageView.anchor(top: nil, left: self.leftAnchor, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 8, paddingBottom: 0, paddingRight: 0, width: 40, height: 40)
         profileImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
         profileImageView.layer.cornerRadius = 40 / 2
         
-        addSubview(followButton)
-        followButton.anchor(top: nil, left: nil, bottom: nil, right: self.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 12, width: 90, height: 30)
-        followButton.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-        followButton.layer.cornerRadius = 3
-        followButton.isHidden = true
         
-        self.addSubview(postImageView)
-        postImageView.anchor(top: nil, left: nil, bottom: nil, right: self.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 8, width: 40, height: 40)
-        postImageView .centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-        
-        self.addSubview(notificationLabel)
-        notificationLabel.anchor(top: nil, left: profileImageView.rightAnchor, bottom: nil, right: postImageView.leftAnchor, paddingTop: 0, paddingLeft: 8, paddingBottom: 0, paddingRight: 8, width: 0, height: 0)
-        notificationLabel .centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
     }
     
     required init?(coder: NSCoder) {
